5e5d6a934b8f340f87f7ae04d752f12eb74dd43d
mendurim <mendurim.r@gmail.com>
2016-03-12 21:29:33 +0100

    Added js function

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ $(document).ready(function(){
 		$(this).parent().addClass('current'); 
 
 		// Set heading text
-		$('h1.heading').text($(this).text()); 
+		$('#heading').text($(this).text()); 
+
+		// Get & filter link text
+		var category = $(this).text().toLowerCase().replace(' ', '-'); 
+
+		// Remove hidden class if 'all projects' is selected
+		if(category== 'all-projects'){
+			$('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden'); 
+		} else{
+			$('ul#gallery li').each(function(){
+				if(!$(this).hasClass(category)){
+					$(this).hide().addClass('hidden'); 
+				}else {
+					$(this).fadeIn('slow').removeClass('hidden'); 
+				}
+			}); 
+		}
+		// Stop link behaviour
+		return false; 
+
 	}); 
 }); 
